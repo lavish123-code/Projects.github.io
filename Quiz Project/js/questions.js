a64fe8488b2f53755e9ab66aafbafc01bc369098
Lavish Garg <69461656+lavish123-code@users.noreply.github.com>
2021-01-24 14:57:56 +0530

    Add files via upload

---
+++ Quiz Project/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the scientific name of a butterfly?",
+    answer: "Rhopalocera",
+    options: [
+      "Apis",
+      "Coleoptera",
+      "Formicidae",
+      "Rhopalocera"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How hot is the surface of the sun?",
+    answer: "5,778 K",
+    options: [
+      "1,233 K",
+      "5,778 K",
+      "12,130 K",
+      "101,300 K"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of Spain?",
+    answer: "Madrid",
+    options: [
+      "Berlin",
+      "Buenos Aires",
+      "Madrid",
+      "San Juan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is 70 degrees Fahrenheit in Celsius?",
+    answer: "21.1111",
+    options: [
+      "18.8889",
+      "20",
+      "21.1111",
+      "158"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When was Mahatma Gandhi born?",
+    answer: "October 2, 1869",
+    options: [
+      "October 2, 1869",
+      "December 15, 1872",
+      "October 2, 1878",
+      "October 2, 1769"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How far is the moon from Earth?",
+    answer: "238,400 miles (384,400 km)",
+    options: [
+      "7,918 miles (12,742 km)",
+      "86,881 miles (139,822 km)",
+      "238,400 miles (384,400 km)",
+      "35,980,000 miles (57,910,000 km)"
+    ]
+  },
+  {
+    numb: 7,
+    question: "When did The Avengers come out?",
+    answer: "May 4, 2012",
+    options: [
+      "May 2, 2008",
+      "May 4, 2012",
+      "May 3, 2013",
+      "April 4, 2014"
+      ]
+  },
+  {
+    numb: 8,
+    question: "How tall is Mount Everest?",
+    answer: "29,029 ft (8,847 m)",
+    options: [
+      "6,683 ft (2,037 m)",
+      "7,918 ft (2,413 m)",
+      "19,341 ft (5,895 m)",
+      "29,029 ft (8,847 m)"
+      ],
+  },
+  {
+    numb: 9,
+    question: "What is 65 times 52?",
+    answer: "3380",
+    options:[
+      "117",
+      "3120",
+      "3380",
+      "3520"
+      ]
+  },
+  {
+    numb: 10,
+    question: "What are the school colors of the University of Texas at Austin?",
+    answer: "White, Burnt Orange",
+    options: [
+      "Black, Red",
+      "Blue, Orange",
+      "White, Burnt Orange",
+      "White, Old gold, Gold"
+      ]
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
